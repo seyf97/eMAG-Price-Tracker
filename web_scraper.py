4f0492796deed45644f192d9a21692a5d35c2e54
seyf97 <111386377+seyf97@users.noreply.github.com>
2023-11-07 20:15:08 +0200

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -2,17 +2,16 @@ from bs4 import BeautifulSoup
 import requests
 
 HEADERS = {
-    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0",
-    "Accept" : "text/html,application/xhtml",
-    "Accept-Language" : "en-US,en;q=0.9,de-DE;q=0.8,de;q=0.7,ro;q=0.6,tr;q=0.5",
-    "Accept-Encoding" : "gzip, br",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.2 Safari/605.1.15",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests" : "1",
-    "Sec-Fetch-Dest" : "iframe",
-    "Sec-Fetch-Mode" : "navigate",
-    "Sec-Fetch-Site" : "same-origin",
-    "Sec-Fetch-User" : "?1",
-    "Cache-Control" : "max-age=0"}
+    "Upgrade-Insecure-Requests": "1",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache"
+}
+
 
 class Web_Scraper:
 
